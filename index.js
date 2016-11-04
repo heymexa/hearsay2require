7288f8b13a8c093bb675b646e6370622af6edb90
heymexa <heymexa@gmail.com>
2016-11-04 13:07:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const requireJsConfig = {
+    waitSeconds: 300
+};
+
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const hearsayConfigFile = process.argv[2];
+let requireJsTemplate = 'requirejs.config(%config%);';
+
+const getRequireJs = (hearsay) => {
+    const options = hearsay.hearsay_require_js.optimizer.options;
+    for (let option in options) {
+        if (options.hasOwnProperty(option)) {
+            requireJsConfig[option] = options[option];
+        }
+    }
+    return requireJsConfig;
+};
+
+const saveRequireJsConfig = (hearsay, commonJs) => {
+    const fileContent = requireJsTemplate.replace('%config%', JSON.stringify(commonJs));
+    fs.writeFile(hearsay.hearsay_require_js.common_js_path, fileContent, (err) => {
+        if (err) throw err;
+    });
+};
+
+const convert = () => {
+    try {
+        const hearsay = yaml.safeLoad(fs.readFileSync(hearsayConfigFile, 'utf8'));
+        const requireJs = getRequireJs(hearsay);
+        saveRequireJsConfig(hearsay, requireJs);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+module.exports = convert;
